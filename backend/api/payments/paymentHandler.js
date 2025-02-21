1620d717f1a73f79ae2065a3eb8fa1599374c065
sanskar <sanskaryeeha@gmail.com>
2025-02-22 01:59:16 +0530

    feat: basic payeent integration completed

--- backend/api/payments/paymentHandler.js
+++ backend/api/payments/paymentHandler.js
@@ -4,4 +4,5 @@ const verifyOrder = require("./verifyOrder");
 
 const paymentRouter = express.Router();
 paymentRouter.post("/pay", createOrder);
+paymentRouter.post("/verify", verifyOrder);
 module.exports = paymentRouter;
--- backend/middlerwares/signature.js
+++ backend/middlerwares/signature.js
@@ -1,10 +1,10 @@
 const crypto = require("crypto");
 const config = require("../config/config.js");
 
-const generateSignature = (order_id, payment_id) => {
+const signature = (order_id, payment_id) => {
   return crypto
     .createHmac("sha256", config.razorpay.key_secret)
     .update(order_id + "|" + payment_id)
     .digest("hex");
 };
-module.exports = generateSignature();
+module.exports = signature;
